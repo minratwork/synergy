94371c92ca42f7b8bb1cd57d3e4fc71b0cb6a9f3
minratwork <minrat.work@gmail.com>
2023-01-02 07:29:43 +0700

    add

--- app/Http/Controllers/CovidController.php
+++ app/Http/Controllers/CovidController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CovidCases;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Request;
 
@@ -10,7 +12,7 @@ class CovidController extends Controller
     public function getCovidCasesByCountry(Request $request)
     {
         $countryCases = $this->buildCountryCasesData($request->countryCases);
-     
+        $this->saveCountryCases($countryCases);
         return $countryCases;
     }
 
@@ -20,5 +22,20 @@ class CovidController extends Controller
             'country' => $countryCases['country'],
             'cases' => $countryCases['timeline']['cases']
         ];
+    }
+
+    private function saveCountryCases($countryCases)
+    {
+        // dd($countryCases);
+        
+        foreach($countryCases['cases'] as $key => $case) {
+            $formattedDate = Carbon::parse($key);
+            // dd($key, $t);
+            CovidCases::updateOrCreate([
+                'country' => $countryCases['country'],
+                'date' => $formattedDate,
+                'case' => $case
+            ]);
+        }
     }
 }
